8dd6ded0059619984bc56e0f946df2b465af0fc0
ruugong <smart.nwachukwu@gmail.com>
2020-09-17 09:00:26 -0400

    initial setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Devops-Automation-Plane
+* A Box with the tools needed for DevOps Automation
+
+# tools included
+* Terraform 11.11
+* AWS CLI
+* Ansible
+* Packer
+* Docker
+* Python3X
+* Kubectl
+* Python 2 X
+* curl
+* wget
+* OS = linux
---
+++ scripts/install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -x
+
+TERRAFORM_VERSION="0.12"
+PACKER_VERSION="1.3.3"
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/mykey ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
+# install packages
+apt-get update
+apt-get -y install docker.io ansible unzip
+# add docker privileges
+usermod -G docker ubuntu
+# install pip
+pip install -U pip && pip3 install -U pip
+if [[ $? == 127 ]]; then
+    wget -q https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+    python3 get-pip.py
+fi
+# install awscli and ebcli
+pip install -U awscli
+pip install -U awsebcli
+
+#terraform
+T_VERSION=$(terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+# Kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# clean up
+apt-get clean
